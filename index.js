cca47b5af3a0b8a57d9cb237c264b240d60ca89c
Tahirasma408 <119299889+Tahirasma408@users.noreply.github.com>
2024-03-17 10:42:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-/*import inquirer from "inquirer";
 
-const answer = await inquirer.prompt([
-    {message:"Enter your first no ", type:"number",name:"firstNumber"},
-    {message:"Enter your second no", type:"number",name:"secondNumber"},
-    {
-        message:"Select one of the operator to perform operation",
-        type: "list",
-        name:"operator",
-        choices:["Addition","Subtraction","Multiplication","Division"],
-    },
-]);
-
-// conditional statement
-if (answer.operator=== "Addition"){
-    console.log(answer.firstNumber + answer.secondNumber);
-} else if(answer.operator === "Subtraction"){
-    console.log(answer.firstNumber - answer.secondNumber);
-}else if(answer.operator === "Multiplication"){
-    console.log(answer.firstNumber * answer.secondNumber);
-}else if(answer.operator === "Division"){
-    console.log(answer.firstNumber / answer.secondNumber)
-}else {
-    console.log("please select valid number")
-}
-*/
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
     { message: "Enter your first no", type: "number", name: "firstNumber" },
